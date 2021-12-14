f8a185a6c9baec0430ce94c531a85467b83fea5a
Pete Latham <pete@cothamtechnologies.com>
2021-12-14 17:17:20 +0000

    Playing

--- server.js
+++ server.js
@@ -2,141 +2,88 @@ const express = require('express');
 const axios = require('axios');
 
 const wrapperServer = express();
-let parser = require('body-parser');
+wrapperServer.use(express.urlencoded({ extended: true }));
+wrapperServer.use(express.json());
 
-wrapperServer.use(parser.json());
+// let parser = require('body-parser');
+// wrapperServer.use(parser.json());
 
-wrapperServer.get('/entities', async (req, res) => {
-  async function getResponseStructure() {
-    let responseStructure = {};
+let queryData = JSON.stringify({
+  query: `{
+  getEntities {
+    id
+    title
+  }
+}`,
+  variables: {},
+});
 
-    req.body.entities.forEach(async (entityName) => {
-      function matchItemName(item) {
-        return item.title === entityName;
-      }
+function matchItemName(item) {
+  return item.title === entityName;
+}
 
-      function matchAttributesAndValues(attributes, values) {
-        let returnStructure = [];
+function matchAttributesAndValues(attributes, values) {
+  let returnStructure = [];
 
-        values.data.forEach((valuesSet) => {
-          let record = {};
+  values.data.forEach((valuesSet) => {
+    let record = {};
 
-          valuesSet.attributes.forEach((value) => {
-            let attr = attributes.filter((attribute) => attribute.id === value.id);
-            attrName = attr[0].name;
+    valuesSet.attributes.forEach((value) => {
+      console.log('🚀 ~ file: server.js ~ line 31 ~ valuesSet.attributes.forEach ~ value', value);
+      let attr = attributes.filter((attribute) => attribute.id === value.id);
+      attrName = attr[0].name;
 
-            record[`${attrName}`] = value.value;
-          });
-
-          returnStructure.push(record);
-        });
-
-        return returnStructure;
-      }
-
-      let data = JSON.stringify({
-        query: `{
-        getEntities {
-          id
-          title
-        }
-      }`,
-        variables: {},
-      });
-
-      const config = {
-        method: 'post',
-        url: 'https://intelastel.io/graphql',
-        headers: {
-          Application: req.headers.application,
-          Authorization: req.headers.authorization,
-          'Content-Type': 'application/json',
-        },
-        data: data,
-      };
-
-      const response = await axios(config);
-      const entityID = response.data.data.getEntities.filter(matchItemName)[0].id;
-
-      responseStructure[entityName] = {};
-      responseStructure[entityName].id = entityID;
-
-      attributeData = JSON.stringify({
-        query: `{
-        Api {
-          getAttributes(entityId: "${entityID}") {
-            attributes {
-              description
-              id
-              label
-              name
-              required
-              type
-            }
-          }
-        }
-      }`,
-      });
-
-      const attributeConfig = {
-        method: 'post',
-        url: 'https://intelastel.io/graphql',
-        headers: {
-          Application: req.headers.application,
-          Authorization: req.headers.authorization,
-          'Content-Type': 'application/json',
-        },
-        data: attributeData,
-      };
-
-      const attributesResponse = await axios(attributeConfig);
-      const attributes = attributesResponse.data.data.Api.getAttributes.attributes;
-
-      const getEntityValuesQuery = JSON.stringify({
-        query: `{
-          Api {
-            getEntityValues(entityId: "${entityID}") {
-              count
-              data {
-                attributes {
-                  id
-                  value
-                }
-              }
-            }
-          }
-        }`,
-      });
-
-      let getEntityValuesConfig = {
-        method: 'post',
-        url: 'https://intelastel.io/graphql',
-        headers: {
-          Application: req.headers.application,
-          Authorization: req.headers.authorization,
-          'Content-Type': 'application/json',
-        },
-        data: getEntityValuesQuery,
-      };
-
-      const valuesResponse = await axios(getEntityValuesConfig);
-      const values = valuesResponse.data.data.Api.getEntityValues;
-      responseStructure[entityName].count = values.count;
-      responseStructure[entityName].data = [];
-
-      responseStructure[entityName].data = matchAttributesAndValues(attributes, values);
-      console.log(
-        '🚀 ~ file: server.js ~ line 128 ~ req.body.entities.forEach ~ responseStructure',
-        JSON.stringify(responseStructure),
-      );
+      record[`${attrName}`] = value.value;
     });
 
-    return responseStructure;
+    returnStructure.push(record);
+  });
+
+  return returnStructure;
+}
+
+wrapperServer.get('/entities', (req, res) => {
+  const config = {
+    method: 'post',
+    url: 'https://intelastel.io/graphql',
+    headers: {
+      Application: req.headers.application,
+      Authorization: req.headers.authorization,
+      'Content-Type': 'application/json',
+    },
+    data: queryData,
+  };
+
+  async function getResponseStructure() {
+    let responseStructure = {
+      default: 'Here we go ...',
+    };
+
+    req.body.entities.forEach(async (entityName) => {
+      console.log('🚀 ~ file: server.js ~ line 18 ~ req.body.entities.forEach ~ entityName', entityName);
+
+      try {
+        const response = await axios(config);
+        // console.log('response');
+        // console.log(response);
+        const entityID = response.data.data.getEntities.filter(matchItemName(entityName))[0].id;
+        console.log('🚀 ~ file: server.js ~ line 68 ~ getResponseStructure ~ entityID', entityID);
+      } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+      }
+    });
+    return { entityID };
   }
 
-  const result = await getResponseStructure();
+  getResponseStructure()
+    .then((data) => {
+      console.log('🚀 ~ file: server.js ~ line 75 ~ .then ~ data', data);
 
-  res.json(result);
+      res.send(data);
+    })
+    .catch((error) => res.status(500).send(error))
+    .finally(() => console.log('All done'));
 });
 
 const port = 4001;
